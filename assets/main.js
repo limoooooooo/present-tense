e8607e3c0416a30e3e550872092bdff3ac6b75b2
limoooooooo <102713325+limoooooooo@users.noreply.github.com>
2025-08-22 17:20:29 +1200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+// function showNext1() {
+//   document.getElementById("line-1").style.display = "inline"; 
+// }
+
+// function showNext2() {
+//   document.getElementById("line-2").style.display = "inline"; 
+// }
+
+// function showNext3() {
+//   document.getElementById("line-3").style.display = "inline"; 
+// }
