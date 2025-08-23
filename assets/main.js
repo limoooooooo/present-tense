3a7f8b126615bfe454e3f27407f4dbc2b95f80b8
limoooooooo <102713325+limoooooooo@users.noreply.github.com>
2025-08-23 13:18:28 +1200

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,11 +1,67 @@
-// function showNext1() {
-//   document.getElementById("line-1").style.display = "inline"; 
-// }
+// favicon randomiser
 
-// function showNext2() {
-//   document.getElementById("line-2").style.display = "inline"; 
-// }
+let faviconElem = document.getElementById('favicon');
+    let randomNumber = Math.floor(Math.random() * 7) + 1;
+    faviconElem.setAttribute('href', `assets/favicon/favi-${randomNumber}.png`);
 
-// function showNext3() {
-//   document.getElementById("line-3").style.display = "inline"; 
-// }
+
+// textLoader
+
+    function showNext1() {
+    var typed = new Typed('#line-1', {
+      stringsElement: '#string-1',
+      typeSpeed: 3,
+      showCursor: false,
+    });
+   }
+
+   function showNext2() {
+    var typed = new Typed('#line-2', {
+      stringsElement: '#string-2',
+      typeSpeed: 3,
+      showCursor: false,
+    });
+   }
+
+   function showNext3() {
+    var typed = new Typed('#line-3', {
+      stringsElement: '#string-3',
+      typeSpeed: 3,
+      showCursor: false,
+    });
+   }
+
+   function showNext4() {
+    var typed = new Typed('#line-4', {
+      stringsElement: '#string-4',
+      typeSpeed: 3,
+      showCursor: false,
+    });
+   }
+
+   function showNextLiam() {
+    var typed = new Typed('#line-liam', {
+      stringsElement: '#string-liam',
+      typeSpeed: 3,
+      showCursor: false,
+    });
+   }
+
+// clock
+
+   const { DateTime } = luxon;
+    
+
+    function updateTime() {
+      const aucklandTimeElement = document.getElementById('auckland-time');
+
+      const now = DateTime.local();
+      const aucklandTime = now.setZone('Pacific/Auckland');
+      
+
+      aucklandTimeElement.textContent = `${aucklandTime.toLocaleString(DateTime.TIME_WITH_SECONDS)} NZST`;
+    }
+
+    // Initial call and then update every second
+    updateTime();
+    setInterval(updateTime, 1000);
